5298d24ba958f14526b9b7981ad276cb0bd5ad62
valursp <vsp@hi.is>
2014-01-31 14:20:27 +0000

    Sparireikningur klasi

---
+++ Spara.py
@@ -0,0 +1,24 @@
+# Notkun: x = Spara(eign, innb, mark, tegund)
+# Fyrir: Ekkert
+# Eftir: Nyr Sparireikningur med tilheyrandi eiginleikum
+class Spara:
+
+    #Bua til nyjan reikning
+    def __init__(self, eign, innb, mark, tegund):
+        self.eign = eign
+        self.innb = innb
+        self.mark = mark
+        if (tegund == 0):   #Tegund 0 er heidursmerki
+            self.vextir = 4.2
+            self.verdt = 0.0
+        elif (tegund == 1): #Tegunr 1 er Sparileid 1
+            self.vextir = 1.7
+            self.verdt = 4;
+        elif (tegund == 2): #Tegund 2 er Sparileid 2
+            self.vextir = 1.8
+            self.verdt = 4
+        elif (tegund == 3): #Tegund 3 er Sparileid 3
+            self.vextir = 1.8
+            self.verdt = 4;
+
+
