76ac144289015a113f23ccc61085ed39b447ed89
gudrun89 <gudrun89@gmail.com>
2014-02-10 12:28:31 +0000

    principalAfterMonths fall
    Núna er hægt að sjá hvernig lán þróast með tímanum

--- Loan.py
+++ Loan.py
@@ -12,7 +12,7 @@ class Loan:
         if self.months is None:
             self.months = self.monthsToPay(float(self.monthPaym))
         if self.monthPaym is None:
-            self.monthPaym = self.paymentPerMonth(float(self.months))
+            self.monthPaym = self.paymentPerMonth(self.principal, float(self.months))
             
 
     def makePayment(self, amount):
@@ -26,5 +26,20 @@ class Loan:
 
     # months = length of loan
     # returns the payment to be paid each month
-    def paymentPerMonth(self, months):
-        return self.principal*(self.interest +  self.interest/((1+self.interest)**months - 1))
+    def paymentPerMonth(self, principal, months):
+        return principal*(self.interest +  self.interest/((1+self.interest)**months - 1))
+
+    def principalAfterMonths(self, months):
+        if (months == 1):
+            return self.principal - self.paymentPerMonth(self.principal, self.months)
+        return self.principalAfterMonths(months-1) - self.paymentPerMonth(self.principalAfterMonths(months-1), self.months-(months-1))
+
+
+    def loanDevelopment(self, months):
+        return [principalAfterMonths(i) for i in range(1,months+1)]
+
+    def plotLoanDevelopment(self, months):
+        months = [i for i in range(1,months+1)]
+        principals = self.loanDevelopment(months)
+        line, = plt.plot(months, principals)
+        
